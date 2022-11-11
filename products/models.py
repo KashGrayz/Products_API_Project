16b993e75a9691165f47661d8aae7819b5157eae
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-11 13:42:36 -0500

    Created New Column in Database

--- products/models.py
+++ products/models.py
@@ -7,4 +7,5 @@ class Product(models.Model):
     discription = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image_url = models.CharField(max_length=255, null=None)
     
--- products/serializers.py
+++ products/serializers.py
@@ -6,4 +6,4 @@ from .models import Product
 class ProductSerialiszer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'discription', 'price', 'inventory_quantity']
+        fields = ['id', 'title', 'discription', 'price', 'inventory_quantity', 'image_url']
